b3b1109bd7926e75d85a7f91a72cd3789392960d
camargolucas11 <lucascammargo19@gmail.com>
2024-03-09 00:10:48 -0300

    previsão do tempo

---
+++ main.js
@@ -0,0 +1,5 @@
+function getWeather() {
+    const apiKey = '1ccc8bf295a0fb23276f06e9f3a9b186';
+
+    const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric';
+}
