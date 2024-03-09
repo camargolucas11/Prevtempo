6282166ec80f9d923a806be30902366696ca1512
camargolucas11 <lucascammargo19@gmail.com>
2024-03-09 00:38:22 -0300

    resultado fetch

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 function getWeather() {
     const apiKey = '1ccc8bf295a0fb23276f06e9f3a9b186';
 
-    const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric';
-}
+    const apiLink = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+    fetch(apiLink)
+    .then(Response => Response.json())
+    .then(data => {
+        const cidadeNome = data.name;
+        const temperatura = data.main.temp;
+        const wheatherDesc = data.wheather[0].description;
+
+        const wheatherDiv = document.getElementById('wheather');
+        
+    })
